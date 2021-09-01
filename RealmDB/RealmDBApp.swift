63f4d8dd62c9c8666ba270aa9d47ed63f2209ad9
gitnadia <nadia.g@inbox.lv>
2021-09-01 18:22:15 +0300

    Initial Commit

---
+++ RealmDB/RealmDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmDBApp.swift
+//  RealmDB
+//
+//  Created by nadezda.gura on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
